0c93f4f8c41e17024d482f54f6b1b0dd92f69c65
Leonardo Caloni <leomunduruca@gmail.com>
2023-09-27 12:25:43 -0300

    BINARY NUMBER TO DECIMAL NUMBER VERIFICATION

--- Binary_decimal.java
+++ Binary_decimal.java
@@ -2,11 +2,19 @@ import javax.swing.JOptionPane;
 
 public class Binary_decimal {
     public static void main(String args[]) {
-        String binaryStr = JOptionPane.showInputDialog(null, "Enter a binary number to transform it into a decimal number:",
-                        "Conversion of binary numbers", JOptionPane.PLAIN_MESSAGE);
+        String binaryStr = JOptionPane.showInputDialog(null,
+                "Enter a binary number to transform it into a decimal number:",
+                "Conversion of binary numbers", JOptionPane.PLAIN_MESSAGE);
+        while (binaryStr.contains("1") == false && binaryStr.contains("0") == false) {
+            JOptionPane.showMessageDialog(null, "Please, enter a binary number.",
+                    "Error", JOptionPane.WARNING_MESSAGE);
+            binaryStr = JOptionPane.showInputDialog(null,
+                "Enter a binary number to transform it into a decimal number:",
+                "Conversion of binary numbers", JOptionPane.PLAIN_MESSAGE);
+        }
         int len = binaryStr.length();
         int result = 0;
-        for (int i = len-1; i >= 0; i--) {
+        for (int i = len - 1; i >= 0; i--) {
             char digit = binaryStr.charAt(i);
             int digitValue = Character.getNumericValue(digit);
             result += digitValue * Math.pow(2, len - 1 - i);
